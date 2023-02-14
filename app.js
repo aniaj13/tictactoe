48dc2139668bf9734180cbad4735e53eafa3383d
Anna Jaśkiewicz <ania.jaskiewicz.2001@gmail.com>
2023-02-14 22:01:18 +0100

    declared winning combinations and added reset button

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const board = document.getElementById('board');
 const resetBtn = document.getElementById('resetBtn');
+const startBtn = document.getElementById('startBtn');
 let playerTurnSign = document.getElementById('playerTurnSign');
+let gameInfo = document.getElementById('gameInfo');
+const squareA = document.getElementById('square_a');
+const squareB = document.getElementById('square_b');
+const squareC = document.getElementById('square_c');
+const squareD = document.getElementById('square_d');
+const squareE = document.getElementById('square_e');
+const squareF = document.getElementById('square_f');
+const squareG = document.getElementById('square_g');
+const squareH = document.getElementById('square_h');
+const squareI = document.getElementById('square_i');
 
 const squares = document.getElementsByClassName('square');
 
-let playerOTurn = true;
-playerTurnSign.innerText = 'O';
-for (let square of squares) {
-   square.addEventListener('click', function () {
-       if (playerOTurn) {
-            playerTurnSign.innerText = 'X';
-           square.innerText = 'O';
-           playerOTurn = false;
-       }
-       else {
-           playerTurnSign.innerText = 'O';
-           square.innerText = 'X';
-           playerOTurn = true;
-       }})}
+const winningCombinations = [
+    [squareA, squareD, squareG],
+    [squareB, squareE, squareH],
+    [squareC, squareF, squareI],
+    [squareA, squareB, squareC],
+    [squareD, squareE, squareF],
+    [squareG, squareH, squareI],
+    [squareA, squareE, squareI],
+    [squareC, squareE, squareG],
+];
+
 
