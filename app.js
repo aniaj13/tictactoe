88b8cdc728c58ff617900f2001057189135883df
Anna Jaśkiewicz <ania.jaskiewicz.2001@gmail.com>
2023-02-17 18:43:20 +0100

    created a form to pick a game mode and implemented a play with pc game mode

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-const board = document.getElementById('board');
 const resetBtn = document.getElementById('resetBtn');
 const startBtn = document.getElementById('startBtn');
-let playerTurnSign = document.getElementById('playerTurnSign');
-let winnerInfo = document.getElementById('winnerInfo');
-let gameInfo = document.getElementById('gameInfo');
 const squareA = document.getElementById('square_a');
 const squareB = document.getElementById('square_b');
 const squareC = document.getElementById('square_c');
@@ -13,8 +9,14 @@ const squareF = document.getElementById('square_f');
 const squareG = document.getElementById('square_g');
 const squareH = document.getElementById('square_h');
 const squareI = document.getElementById('square_i');
+let playerTurnSign = document.getElementById('playerTurnSign');
+let winnerInfo = document.getElementById('winnerInfo');
+let gameInfo = document.getElementById('gameInfo');
+const pcChoiceBtn = document.getElementById('pcPlayer')
+const playerChoiceBtn = document.getElementById('otherPlayer')
 
 const squares = document.getElementsByClassName('square');
+const squaresArr = Array.from(squares);
 
 const winningCombinations = [
     [squareA, squareD, squareG],
@@ -30,6 +32,7 @@ const winningCombinations = [
 let isGameOn = false;
 let playerOTurn = false;
 gameInfo.style.display = 'none';
+let gameMode = null;
 
 
 function resetGame() {
@@ -38,12 +41,75 @@ function resetGame() {
     isGameOn = true;
     playerOTurn = true;
     playerTurnSign.innerText = 'O';
-    for (let square of squares) {
+    for (let square of squaresArr) {
         square.innerText = '';
-        square.addEventListener('click', makeMove)
-}}
-startBtn.addEventListener('click', resetGame);
+    }
+}
+
 resetBtn.addEventListener('click', resetGame);
+startBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (validateForm()) {
+        resetGame();
+        let mode = checkGameModeChoice()
+        if (mode === 'player') {
+            gameMode = 'player'
+                for (let square of squaresArr) {
+                    square.addEventListener('click', makeMove)
+                }}
+        else if (mode === 'pc') {
+            gameMode = 'pc';
+                for (let square of squaresArr) {
+                    square.addEventListener('click', playWithPc);
+                }}}
+});
+
+function playWithPc() {
+    if (playerOTurn === true) {
+        playerTurnSign.innerText = 'X';
+        this.innerText = 'O';
+        playerOTurn = false;
+        pcMove();
+        playerOTurn = true;
+    }
+    this.removeEventListener('click', playWithPc)
+    let win = checkWinner();
+    if (!win) {
+        checkTie();
+    }
+}
+
+function pcMove() {
+        let pcTurn = true
+        while (pcTurn) {
+            let i = createRandNum();
+            for (let square of squaresArr) {
+                if (checkEmpty(squaresArr[i])) {
+                    squaresArr[i].innerHTML = 'X';
+                    squaresArr[i].removeEventListener('click', playWithPc)
+                    pcTurn = false;
+                } else if (isBoardFull()) {
+                    pcTurn = false;
+                }
+            }}}
+
+function checkEmpty(square) {
+    if (square.innerText !== 'X' && square.innerText !== 'O') {
+        return true
+    } return false;
+}
+
+function isBoardFull() {
+    for (let i = 0; i < squaresArr.length; i++) {
+         if (checkEmpty(squaresArr[i])) {
+             return false
+         }
+    } return true;
+}
+
+function createRandNum() {
+    return Math.floor(Math.random() * 9);
+}
 
 function makeMove() {
     if (playerOTurn === true) {
@@ -56,11 +122,14 @@ function makeMove() {
         playerOTurn = true;
     }
     this.removeEventListener('click', makeMove);
-    checkWinner();
+    let win = checkWinner();
+    if (!win){
+        checkTie();
+    }
 }
 
 function removeSquareEvents() {
-    for (let square of squares) {
+    for (let square of squaresArr) {
         square.removeEventListener('click', makeMove)
     }
 }
@@ -88,14 +157,11 @@ function checkWinner(){
             winnerInfo.innerText = 'GAME OVER';
             winnerInfo.innerText += ` Player ${winner} Won!`
             removeSquareEvents();
-        }}
-        if (checkTie()) {
-            console.log('Its a tie!');
-            gameInfo.style.display = 'none'
-            winnerInfo.style.display = 'block'
-            winnerInfo.innerText = 'Its a tie!'
-    }
+            return true;
+        }
+    } return false;
 }
+
 
 function whoWon(array) {
     let first = array[0].innerText;
@@ -110,10 +176,31 @@ function whoWon(array) {
 }
 
 function checkTie() {
-    for (let square of squares) {
+    for (let square of squaresArr) {
             if (square.innerText === '') {
                 return false;
             }
-    } return true;
+    }   isGameOn = false;
+        console.log('Its a tie!')
+        gameInfo.style.display = 'none'
+        winnerInfo.style.display = 'block'
+        winnerInfo.innerText = 'Its a tie!'
+        removeSquareEvents();
 }
 
+
+function validateForm() {
+    if (!pcChoiceBtn.checked && !playerChoiceBtn.checked) {
+        alert('pick a game mode')
+        return false;}
+    return true;
+}
+function checkGameModeChoice() {
+    if (pcChoiceBtn.checked) {
+        console.log('you selected pc player');
+        return 'pc';
+    } else if (playerChoiceBtn.checked) {
+        console.log('you selected other player');
+        return 'player';
+    }
+}
